c9648928f7039a08f189e546a9c3f7eb4ec20387
Pawel Pierzchala <pawelpierzchala@gmail.com>
2013-07-19 13:29:48 +0200

    Add template for roman numerals exercise

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "rspec"
+gem "bogus"
+gem "guard-rspec"
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/roman_numerals'
+require 'spec_helper'
+
+describe "Roman numerals calculator" do
+  it "sums I and I to II" do
+    fail
+  end
+end
+
